3571651fbe5a0b91045e4a189d8d45582b8d2585
blackay <3012608231@qq.com>
2017-06-24 15:31:52 +0800

    Initial Commit

---
+++ wei_shen/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  wei_shen
+//
+//  Created by MAC on 2017/6/24.
+//  Copyright © 2017年 blackay. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
